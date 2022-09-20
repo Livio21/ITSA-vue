e03fc4c89998ff410093a1d3ef44449fb25271d6
Livio21 <livio.macaj@cit.edu.al>
2022-09-20 19:22:06 +0200

    ab

--- src/App.vue
+++ src/App.vue
@@ -46,6 +46,9 @@ export default {
           loaded.value = true;
         }
       }
+      window.addEventListener("beforeunload", () => {
+        store.dispatch("logout");
+      });
     })
     return {
       store, loaded
